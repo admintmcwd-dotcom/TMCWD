4769d3d192f1624b2d53b392de3550786a4fad4b
admintmcwd-dotcom <admin.tmcwd@gmail.com>
2026-04-22 11:17:08 +0800

    Add files via upload

---
+++ TMCWD.Application/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TMCWD.Application.Models
+{
+    public class LoginViewModel
+    {
+        #region constructors
+        public LoginViewModel() { }
+        #endregion
+
+        #region properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        #endregion
+    }
+}
