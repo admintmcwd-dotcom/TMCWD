c9fe5a781aff03bcaaf14f063bb1525f1578f93b
kalabhogs <143044628+kalabhogs@users.noreply.github.com>
2026-05-31 13:34:52 +0800

    Add files via upload

---
+++ TMCWD.Data/Services/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using TMCWD.Data.Entities;
+
+namespace TMCWD.Data.Services
+{
+    public interface IAccountService
+    {
+
+        Task<Account> Get(int id);
+        Task<Account> GetByAccountNumber(string accountNumber);
+        Task<IEnumerable<Account>> GetByCustomerId(int customerId);
+        Task<Account> GetByMeterNumber(string meterNumber);
+        Task<Account> SaveUpdate(int userId, Account account);
+        Task<IEnumerable<Account>> GetAccounts();
+    }
+}
