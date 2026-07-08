3feca9df1d77c5f11e4f5a8a12b32302e33bf75f
Wally Villanueva <wallyvillanueva78@gmail.com>
2026-07-08 16:44:59 +0800

    Update 07/08/2026 A

---
+++ TMCWD.Data/Controllers/OtherChargeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TMCWD.Data.Services;
+
+namespace TMCWD.Data.Controllers
+{
+    public class OtherChargeController : Controller
+    {
+
+        #region fields
+
+        private readonly IOtherChargeService _service;
+
+        #endregion
+
+        #region constructors
+
+        public OtherChargeController(IOtherChargeService service)
+        {
+            _service = service;
+        }
+
+        #endregion
+
+        #region methods
+
+        public async Task<IActionResult> Get(int id)
+        {
+            var otherCharges = await _service.Get(id);
+            if (otherCharges == null) return NotFound();
+            return Ok(otherCharges);
+        }
+
+        #endregion
+
+    }
+}
